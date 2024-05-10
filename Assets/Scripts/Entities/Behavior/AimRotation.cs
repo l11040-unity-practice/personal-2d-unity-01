df3f67e52c36a621f59f4f87a4920887dd83608f
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-11 00:14:47 +0900

    Update : Rotate메소드 OnLook에 구독

--- Assets/Scripts/Entities/Behavior/AimRotation.cs
+++ Assets/Scripts/Entities/Behavior/AimRotation.cs
@@ -1,5 +1,20 @@
+using System;
 using UnityEngine;
 
 public class AimRotation : MonoBehaviour
 {
+    private EntityController _controller;
+    private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _controller = GetComponent<EntityController>();
+        _controller.OnLookEvent += Rotate;
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+        float degree = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _spriteRenderer.flipX = Mathf.Abs(degree) > 90f;
+    }
 }
