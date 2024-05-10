89663882e291c8bd895beb000849df8ec67e1ecd
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-10 23:30:24 +0900

    Update : 움직일때 isWalk 트리겨를 변경하는 Move함수 작성 및 구독

--- Assets/Scripts/Entities/Controller/AnimeController.cs
+++ Assets/Scripts/Entities/Controller/AnimeController.cs
@@ -2,4 +2,18 @@ using UnityEngine;
 
 public class AnimeController : MonoBehaviour
 {
+    private EntityController _controller;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _controller = GetComponent<EntityController>();
+        _animator = GetComponent<Animator>();
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _animator.SetBool("isWalk", direction.magnitude > 0);
+    }
 }
