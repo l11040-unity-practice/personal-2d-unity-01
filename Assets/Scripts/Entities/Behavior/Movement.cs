a21e09fcbf344a9bdd53048d20fe24a08c58a381
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-10 22:29:31 +0900

    Update : Movement 움직임 구현

--- Assets/Scripts/Entities/Behavior/Movement.cs
+++ Assets/Scripts/Entities/Behavior/Movement.cs
@@ -2,4 +2,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private EntityController _controller;
+    private Rigidbody2D _rigid;
+    private Vector2 _direction = Vector2.zero;
+    // TODO : 추후 스텟으로 변경해야함
+    private float _speed = 10;
+    private void Awake()
+    {
+        _controller = GetComponent<EntityController>();
+        _rigid = GetComponent<Rigidbody2D>();
+        _controller.OnMoveEvent += Move;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_direction.magnitude > 0)
+        {
+            _rigid.velocity = _direction * _speed;
+        }
+    }
+
+    private void Move(Vector2 direction)
+    {
+        _direction = direction.normalized;
+    }
 }
