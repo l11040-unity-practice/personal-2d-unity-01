1d68d37e7b857e2647c85fcf40118bf34874ccf4
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-13 10:55:14 +0900

    Add : 카메라 이동 추가

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float SmoothSpeed = 0.125f;
+    public Vector3 offset;
+    private void Start()
+    {
+        transform.position = Target.position + offset;
+    }
+    private void FixedUpdate()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+
+        Vector3 desiredPosition = Target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
