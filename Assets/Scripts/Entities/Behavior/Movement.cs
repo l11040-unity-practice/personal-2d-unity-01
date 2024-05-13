854cbcfe22efea0f4abdd5928a9f9d8f321d236b
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-13 13:12:40 +0900

    Update : 기존 speed 스텟에 있는 속도로 변경

--- Assets/Scripts/Entities/Behavior/Movement.cs
+++ Assets/Scripts/Entities/Behavior/Movement.cs
@@ -3,20 +3,21 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private EntityController _controller;
+    private CharacterStatsController _characterStatsController;
     private Rigidbody2D _rigid;
     private Vector2 _direction = Vector2.zero;
-    // TODO : 추후 스텟으로 변경해야함
-    private float _speed = 5;
+
     private void Awake()
     {
         _controller = GetComponent<EntityController>();
+        _characterStatsController = GetComponent<CharacterStatsController>();
         _rigid = GetComponent<Rigidbody2D>();
         _controller.OnMoveEvent += Move;
     }
 
     private void FixedUpdate()
     {
-        _rigid.velocity = _direction * _speed;
+        _rigid.velocity = _direction * _characterStatsController.CurrentStat.Speed;
     }
 
     private void Move(Vector2 direction)
