6eee9d2ed00efc260f95760d82b7681810087cc9
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-13 20:58:07 +0900

    Update : WeaponController 작성

--- Assets/Scripts/Weapon/Controller/WeaponController.cs
+++ Assets/Scripts/Weapon/Controller/WeaponController.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
+    [SerializeField] protected AttackSO AttackData;
+    protected EntityController _controller;
+    protected SpriteRenderer _weaponRenderer;
+    protected Vector2 _direction;
+
+    protected virtual void Awake()
+    {
+        _weaponRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public void InitWeapon(EntityController controller)
+    {
+        _controller = controller;
+        if (_controller)
+        {
+            _controller.OnAttackEvent += Attack;
+            _controller.OnLookEvent += Look;
+        }
+    }
+
+    protected virtual void Look(Vector2 direction)
+    {
+        float degree = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _weaponRenderer.flipY = Mathf.Abs(degree) > 90f;
+        _direction = direction;
+    }
+
+    protected virtual void Attack()
+    {
+    }
 
 }
