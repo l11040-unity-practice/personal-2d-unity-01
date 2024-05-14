780c042632018bf75db9d61711da64dcbbe19376
l11040 <113540683+l11040@users.noreply.github.com>
2024-05-14 21:39:43 +0900

    Update 공격 시 마우스 고정 및 공격 딜레이 추가

--- Assets/Scripts/Entities/Controller/EntityController.cs
+++ Assets/Scripts/Entities/Controller/EntityController.cs
@@ -6,6 +6,24 @@ public class EntityController : MonoBehaviour
     public event Action<Vector2> OnMoveEvent;
     public event Action<Vector2> OnLookEvent;
     public event Action OnAttackEvent;
+    public bool IsAttacking { get; set; }
+    protected bool _isDirectionLocked = false;
+    private float _directionLockDuration = 0.5f;
+    private float _directionLockTimer = float.MaxValue;
+    private float _lastAttackTimer = float.MaxValue;
+    private WeaponEquipSystem _weaponEquipSystem;
+
+    protected virtual void Awake()
+    {
+        _weaponEquipSystem = GetComponent<WeaponEquipSystem>();
+    }
+
+    protected virtual void Update()
+    {
+        HandleDirectionLock();
+        HandleAtteckDelay();
+    }
+
 
     public void CallMoveEvent(Vector2 direction)
     {
@@ -20,5 +38,33 @@ public class EntityController : MonoBehaviour
     public void CallAttackEvent()
     {
         OnAttackEvent?.Invoke();
+    }
+
+
+    private void HandleDirectionLock()
+    {
+        if (_isDirectionLocked)
+        {
+            _directionLockTimer += Time.deltaTime;
+            if (_directionLockTimer >= _directionLockDuration)
+            {
+                _isDirectionLocked = false;
+                _directionLockTimer = 0f;
+            }
+        }
+    }
+    private void HandleAtteckDelay()
+    {
+        if (_weaponEquipSystem.EquipWeaponController == null) return;
+
+        if (_lastAttackTimer < _weaponEquipSystem.EquipWeaponController.AttackData.delay)
+        {
+            _lastAttackTimer += Time.deltaTime;
+        }
+        else if (IsAttacking)
+        {
+            _lastAttackTimer = 0f;
+            CallAttackEvent();
+        }
     }
 }
